ef25ff429523a6909bee912ff5a794743ab6ae0b
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-01-11 09:46:40 -0800

    content update

--- 2019/la-earthquake-app/updateNotebook.sh
+++ 2019/la-earthquake-app/updateNotebook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm notebook-*.js
-curl https://api.observablehq.com/d/a9f353b5513cc8d3.js > notebook.js
+curl -H 'Cache-Control: no-cache' https://api.observablehq.com/d/a9f353b5513cc8d3.js > notebook.js
 python3 version_notebook.py
